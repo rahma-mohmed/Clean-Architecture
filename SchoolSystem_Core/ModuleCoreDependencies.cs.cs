6db75748bc83d0c4e69392ebbee4e4dbbbb88765
rahma-mohmed <engrahma1422003@gmail.com>
2024-12-18 03:36:45 +0200

    Core Dependencies

--- SchoolSystem_Core/ModuleCoreDependencies.cs.cs
+++ SchoolSystem_Core/ModuleCoreDependencies.cs.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SchoolSystem_Core.Behaviors;
 using System.Reflection;
 
 namespace SchoolSystem_Core
 {
-    public static class ModuleCoreDependencies
-    {
-        public static IServiceCollection AddCoreDependencies(this IServiceCollection services)
-        {
-            //Mediator configration
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-            //Mediator automapper
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            return services;
-        }
-    }
+	public static class ModuleCoreDependencies
+	{
+		public static IServiceCollection AddCoreDependencies(this IServiceCollection services)
+		{
+			//Mediator configration
+			services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+			//Mediator automapper
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+			services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+			services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+			return services;
+		}
+	}
 }

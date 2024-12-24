0d43126ed0c8510163291ea6a158268ca57d5aad
rahma-mohmed <engrahma1422003@gmail.com>
2024-12-25 01:18:57 +0200

    Upload Project Files

--- SchoolSystem_Core/obj/Debug/net8.0/SchoolSystem_Core.AssemblyInfo.cs
+++ SchoolSystem_Core/obj/Debug/net8.0/SchoolSystem_Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolSystem_Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13766d49b08f610ff5bfdef69d5e910d4958e27c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8995b409daedaa6f743c95f895f32e7f94ead9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolSystem_Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolSystem_Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

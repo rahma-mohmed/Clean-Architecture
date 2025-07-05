08157323ab5996fc258735930f405985fecabb2a
rahma-mohmed <engrahma1422003@gmail.com>
2025-07-05 18:23:06 +0200

    Authorization service

---
+++ SchoolSystem_Service/IService/IAuthorizationServices.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolSystem_Service.IService
+{
+	public interface IAuthorizationServices
+	{
+		public Task<String> AddRoleAsync(String roleName);
+		public Task<bool> IsRoleExist(String roleName);
+
+	}
+}
---
+++ SchoolSystem_Service/Implementations/AuthorizationServices.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolSystem_Service.IService;
+
+namespace SchoolSystem_Service.Implementations
+{
+	public class AuthorizationServices : IAuthorizationServices
+	{
+		private readonly RoleManager<IdentityRole<int>> _roleManager;
+
+		public AuthorizationServices(RoleManager<IdentityRole<int>> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+
+		public async Task<string> AddRoleAsync(string roleName)
+		{
+			var identityRole = new IdentityRole<int>();
+			identityRole.Name = roleName;
+			var result = await _roleManager.CreateAsync(identityRole);
+
+			if (result.Succeeded)
+			{
+				return "Success";
+			}
+			else
+			{
+				return "Faild";
+			}
+		}
+
+		public async Task<bool> IsRoleExist(string roleName)
+		{
+			var role = await _roleManager.FindByNameAsync(roleName);
+			return role != null;
+		}
+	}
+}
